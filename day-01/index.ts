56a459a670f8186d11fa106595943be6a45f6999
MaeWolff <maxencewolff.pro@gmail.com>
2024-12-01 07:41:57 +0100

    feat: add day 01, part 2

--- day-01/index.ts
+++ day-01/index.ts
@@ -4,7 +4,7 @@ function calculateDifference(number1: number, number2: number) {
   return Math.abs(number1 - number2);
 }
 
-// Solution
+// Solution - Part 1
 
 function createLists() {
   const left = input.split("\n").map((line) => Number(line.split(/\s/)[0]));
@@ -16,8 +16,8 @@ function createLists() {
 function getTotalDistance() {
   const [left, right] = createLists();
 
-  const sortedLeftList = [...left].sort((a, b) => a - b);
-  const sortedRightList = [...right].sort((a, b) => a - b);
+  const sortedLeftList = left.sort((a, b) => a - b);
+  const sortedRightList = right.sort((a, b) => a - b);
 
   return sortedLeftList.reduce(
     (acc, value, index) =>
@@ -26,4 +26,22 @@ function getTotalDistance() {
   );
 }
 
-console.log(getTotalDistance());
+// Solution - Part 2
+
+function getTotalSimilarityScore() {
+  const [left, right] = createLists();
+  const leftCounter: Record<number, number> = {};
+  let total = 0;
+
+  left.forEach((value) => {
+    leftCounter[value] = (leftCounter[value] || 0) + 1;
+  });
+
+  right.forEach((value) => (total += value * leftCounter[value] || 0));
+
+  return total;
+}
+
+// Output
+console.log("Total distance:", getTotalDistance());
+console.log("Total similarity score:", getTotalSimilarityScore());
